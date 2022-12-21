26c007044c72097aa5749fc098446118e2ef116d
sunny kumar singh <sunnyharshsunny12345@gmail.com>
2022-12-21 10:11:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 const app = express();
+
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/globalErrorController");
 const adminRoute = require("./routes/admin.routes");
 
+dotenv.config({ path: "./config.env" })
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PWD);
+
+console.log(DB)
+mongoose.set("strictQuery", false);
+mongoose.connect(DB,{
+  useNew
+})
 
 app.use("/api/v1", adminRoute)
 
